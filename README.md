75af598de257cc96d12a223f41f203cfec2ce346
Kaamil Jasani <kaamilj@yahoo.com>
2020-01-27 15:55:49 +0000

    Fixed latch position, turns out this was wrong too

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ChestLatchFixer [![Latest version][version-image]][version-link]
 
-Fixes the latches on chests so that they don't intersect when animating.  
+Fixes the latches on chests so that they don't slide and intersect when animating.  
 ***Apparently Mojang will not be fixing this issue: https://bugs.mojang.com/browse/MC-170457***
 
 Before:  
@@ -10,6 +10,6 @@ After:
 ![After][after-image]
 
 [before-image]: https://imgur.com/lsmmS4A.png
-[after-image]: https://imgur.com/FHIiqd1.png
+[after-image]: https://imgur.com/rsDW8I8.png
 [version-image]: https://badges.kaamiljasani.com/curseforge/latest/360213
 [version-link]: https://www.curseforge.com/minecraft/mc-mods/chest-latch-fixer
--- src/main/java/com/extracraftx/minecraft/chestlatchfixer/mixin/ChestBlockEntityRendererMixin.java
+++ src/main/java/com/extracraftx/minecraft/chestlatchfixer/mixin/ChestBlockEntityRendererMixin.java
@@ -21,17 +21,17 @@ public class ChestBlockEntityRendererMixin{
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(CallbackInfo info){
         field_20819 = new ModelPart(64, 64, 0, 0);
-        field_20819.addCuboid(7.0F, -1.0F, 14.0F, 2.0F, 4.0F, 1.0F, 0.0F);
+        field_20819.addCuboid(7.0F, -2.0F, 14.0F, 2.0F, 4.0F, 1.0F, 0.0F);
         field_20819.pivotY = 9.0f;
         field_20819.pivotZ = 1.0f;
         
         field_20822 = new ModelPart(64, 64, 0, 0);
-        field_20822.addCuboid(15.0F, -1.0F, 14.0F, 1.0F, 4.0F, 1.0F, 0.0F);
+        field_20822.addCuboid(15.0F, -2.0F, 14.0F, 1.0F, 4.0F, 1.0F, 0.0F);
         field_20822.pivotY = 9.0f;
         field_20822.pivotZ = 1.0f;
         
         field_21481 = new ModelPart(64, 64, 0, 0);
-        field_21481.addCuboid(0.0F, -1.0F, 14.0F, 1.0F, 4.0F, 1.0F, 0.0F);
+        field_21481.addCuboid(0.0F, -2.0F, 14.0F, 1.0F, 4.0F, 1.0F, 0.0F);
         field_21481.pivotY = 9.0f;
         field_21481.pivotZ = 1.0f;
     }
